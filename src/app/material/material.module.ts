bee240e3f2f2441ce2cb7b34d57af6ae23d0281f
Danny Mora <dannyf620@gmail.com>
2018-09-11 11:45:33 -0500

    solve problems

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
-import {MatButtonModule, MatCheckboxModule} from '@angular/material';
-import {MatCardModule} from '@angular/material/card';
+import { MatButtonModule, MatCheckboxModule } from '@angular/material';
+import { MatCardModule } from '@angular/material/card';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDialogModule } from '@angular/material/dialog';
 @NgModule({
-  imports: [MatButtonModule, MatCheckboxModule,MatCardModule],
-  exports: [MatButtonModule, MatCheckboxModule,MatCardModule],
+  imports: [MatButtonModule, MatCheckboxModule, MatCardModule, MatToolbarModule, MatFormFieldModule, MatDialogModule],
+  exports: [MatButtonModule, MatCheckboxModule, MatCardModule, MatToolbarModule, MatFormFieldModule, MatDialogModule],
 })
 export class MaterialModule { }
