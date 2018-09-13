d29acdb95bb806d5f9a5c412238c8e29fa32e1f4
Danny Mora <dfmora@rcnradio.com.co>
2018-09-13 11:06:17 -0500

    add sidemenu

--- src/app/app-routing/routes.ts
+++ src/app/app-routing/routes.ts
@@ -1,3 +1,5 @@
+import { NotFoundComponent } from './../not-found/not-found.component';
+import { MenuComponent } from './../menu/menu.component';
 import { Page2Component } from './../page2/page2.component';
 import { Page1Component } from './../page1/page1.component';
 import { Routes } from '@angular/router';
@@ -5,8 +7,14 @@ import { Routes } from '@angular/router';
 
 
 export const routes: Routes = [
-  { path: 'pagina1',  component: Page1Component },
-  { path: 'pagina2',  component: Page2Component },
-  { path: '', redirectTo: '/pagina1', pathMatch: 'full' },
-  { path: '**', redirectTo: '/', pathMatch: 'full' },
-];
+
+  {
+    path: '', component: MenuComponent, children: [
+      { path: 'pagina1',  component: Page1Component },
+      { path: 'pagina2',  component: Page2Component },
+      { path: '', redirectTo: '/pagina1', pathMatch: 'full' },      
+    ]
+  },
+  { path: '404', component: NotFoundComponent },
+  { path: '**', redirectTo: '/404' }
+]
--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,13 +1,21 @@
 import { NgModule } from '@angular/core';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { MatButtonModule, MatCheckboxModule } from '@angular/material';
+import { MatButtonModule, MatCheckboxModule, MatSidenavModule, MatExpansionModule, MatIconModule, MatListModule } from '@angular/material';
 import { MatCardModule } from '@angular/material/card';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatDialogModule } from '@angular/material/dialog';
 @NgModule({
-  imports: [BrowserAnimationsModule, MatButtonModule, MatCheckboxModule, MatCardModule, MatToolbarModule, MatFormFieldModule, MatDialogModule],
-  exports: [BrowserAnimationsModule, MatButtonModule, MatCheckboxModule, MatCardModule, MatToolbarModule, MatFormFieldModule, MatDialogModule],
+  imports: [BrowserAnimationsModule, MatButtonModule, MatCheckboxModule, MatCardModule, MatToolbarModule, MatFormFieldModule, MatDialogModule,
+    MatSidenavModule,
+    MatExpansionModule,
+    MatIconModule,
+    MatListModule],
+  exports: [BrowserAnimationsModule, MatButtonModule, MatCheckboxModule, MatCardModule, MatToolbarModule, MatFormFieldModule, MatDialogModule,
+    MatSidenavModule,
+    MatExpansionModule,
+    MatIconModule,
+    MatListModule],
 })
 export class MaterialModule { }
